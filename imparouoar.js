023d0f6069639c4c0525fac44068f675cbd4335e
Paulo-o-braga <pdob.lic19@uea.edu.br>
2022-02-16 23:11:34 -0400

    Terceiro commit

---
+++ imparouoar.js
@@ -0,0 +1,15 @@
+/*Paulo de Oliveira Braga
+Licenciatura em Computação
+Matrícula: 1915280021*/
+
+var number;
+var atv;
+number = 20;
+console.log(number);
+
+atv =number%2;
+if(atv%2==0){
+    console.log("O Número é par");
+}else{
+    console.log("Número ímpar");
+}
