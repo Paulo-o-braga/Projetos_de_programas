9569578febf7c783456b61eb63db4d88d3c46684
Paulo-o-braga <pdob.lic19@uea.edu.br>
2022-03-02 23:07:27 -0400

    Decimo terceiro commit

--- atividade3.ts
+++ atividade3.ts
@@ -10,3 +10,27 @@ class Aluno {
         this.numero = numero;
     }
 }
+
+class Professor {
+    public readonly nome: string; //atributo_1
+    public disciplina: string; //atributo_2
+    public qtd_turmas: Number; //atributo_3
+    
+    constructor(nome: string, disciplina: string, qtd_turmas: number){
+        this.nome = nome;
+        this.disciplina = disciplina;
+        this.qtd_turmas = qtd_turmas;
+    }
+}
+
+class Carro {
+    public readonly modelo: string; //atributo_1
+    public fabricante: string; //atributo_2
+    public cor: string; //atributo_3
+    
+    constructor(modelo: string, fabricante: string, cor: string){
+        this.modelo = modelo;
+        this.fabricante = fabricante;
+        this.cor = cor;
+    }
+}
