41cd2115b9add7a9bcb2a40b52ed298ab208429d
Paulo-o-braga <pdob.lic19@uea.edu.br>
2022-03-03 18:35:25 -0400

    Decimo nono commit

--- Exemplos/Exemplo.ts
+++ Exemplos/Exemplo.ts
@@ -1,6 +1,7 @@
-var academico = "Paulo de Oliveira Braga"
-var matricula = 1915280021
-var curso ="Licenciatura em Computação"
+let academico: string = "Paulo de Oliveira Braga"
+let matricula: Number = 1915280021
+let curso: string ="Licenciatura em Computação"
+
 console.log("Nome: ",academico);
 console.log("Matrícula: ",matricula);
 console.log("Curso: ",curso);
