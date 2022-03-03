0ee91710fc2ce8f368aa1a52e763b4a9cf44bd1a
Paulo-o-braga <pdob.lic19@uea.edu.br>
2022-03-03 19:04:00 -0400

    Vigesimo commit

---
+++ Exemplos/exemplo.js
@@ -0,0 +1,6 @@
+/*var academico = "Paulo de Oliveira Braga";
+var matricula = 1915280021;
+var curso = "Licenciatura em Computação";*/
+console.log("Nome: ", academico);
+console.log("Matrícula: ", matricula);
+console.log("Curso: ", curso);
